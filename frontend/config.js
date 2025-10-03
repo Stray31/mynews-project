3ef7762235090c98f35ed5b0ebac0c1f3035eab5
Stray31 <luke.pamular@gmail.com>
2025-10-03 23:56:00 +0800

    apibase

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 window.__API_BASES = {
-  auth: "mynews-project-production-1d2a.up.railway.app",   // replace later with Render URL
-  news: "mynews-project-production-f541.up.railway.app",   // replace later with Render URL
-  email: "mynews-project-production.up.railway.app",
-  user: "mynews-project-production-7de0.up.railway.app"  // replace later with Render URL
+  auth: "https://mynews-project-production-1d2a.up.railway.app",
+  news: "https://mynews-project-production-f541.up.railway.app",
+  email: "https://mynews-project-production.up.railway.app",
+  user: "https://mynews-project-production-7de0.up.railway.app"
 };
